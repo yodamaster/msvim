dbc943f51d74a056dd194a2331a21a2d74a947dc
zbjxb2000@gmail.com <zbjxb2000@gmail.com@289d597c-cd3d-f0c1-17a6-ce0b0cdf5726>
2012-12-19 06:57:21 +0000

    MSDEV 6.0 wizzard created code.

---
+++ msVim/msVim.clw
@@ -0,0 +1,19 @@
+; CLW file contains information for the MFC ClassWizard
+
+[General Info]
+Version=1
+ODLFile=msVim.odl
+ClassCount=1
+Class1=CMsVimApp
+LastClass=CMsVimApp
+NewFileInclude2=#include "msVim.h"
+ResourceCount=0
+NewFileInclude1=#include "stdafx.h"
+LastTemplate=CCmdTarget
+
+[CLS:CMsVimApp]
+Type=0
+HeaderFile=msVim.h
+ImplementationFile=msVim.cpp
+Filter=N
+LastObject=CMsVimApp
---
+++ msVim/msVimTypes.h
@@ -0,0 +1,248 @@
+/* this ALWAYS GENERATED file contains the definitions for the interfaces */
+
+
+/* File created by MIDL compiler version 5.01.0164 */
+/* at Wed Dec 19 14:48:30 2012
+ */
+/* Compiler settings for C:\work\msVim\msVim.odl:
+    Os (OptLev=s), W1, Zp8, env=Win32, ms_ext, c_ext
+    error checks: allocation ref bounds_check enum stub_data 
+*/
+//@@MIDL_FILE_HEADING(  )
+
+
+/* verify that the <rpcndr.h> version is high enough to compile this file*/
+#ifndef __REQUIRED_RPCNDR_H_VERSION__
+#define __REQUIRED_RPCNDR_H_VERSION__ 440
+#endif
+
+#include "rpc.h"
+#include "rpcndr.h"
+
+#ifndef __msVimTypes_h__
+#define __msVimTypes_h__
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+/* Forward Declarations */ 
+
+#ifndef __ICommands_FWD_DEFINED__
+#define __ICommands_FWD_DEFINED__
+typedef interface ICommands ICommands;
+#endif 	/* __ICommands_FWD_DEFINED__ */
+
+
+#ifndef __Commands_FWD_DEFINED__
+#define __Commands_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class Commands Commands;
+#else
+typedef struct Commands Commands;
+#endif /* __cplusplus */
+
+#endif 	/* __Commands_FWD_DEFINED__ */
+
+
+#ifndef __ApplicationEvents_FWD_DEFINED__
+#define __ApplicationEvents_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class ApplicationEvents ApplicationEvents;
+#else
+typedef struct ApplicationEvents ApplicationEvents;
+#endif /* __cplusplus */
+
+#endif 	/* __ApplicationEvents_FWD_DEFINED__ */
+
+
+#ifndef __DebuggerEvents_FWD_DEFINED__
+#define __DebuggerEvents_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class DebuggerEvents DebuggerEvents;
+#else
+typedef struct DebuggerEvents DebuggerEvents;
+#endif /* __cplusplus */
+
+#endif 	/* __DebuggerEvents_FWD_DEFINED__ */
+
+
+void __RPC_FAR * __RPC_USER MIDL_user_allocate(size_t);
+void __RPC_USER MIDL_user_free( void __RPC_FAR * ); 
+
+
+#ifndef __MsVim_LIBRARY_DEFINED__
+#define __MsVim_LIBRARY_DEFINED__
+
+/* library MsVim */
+/* [helpstring][version][uuid] */ 
+
+
+DEFINE_GUID(LIBID_MsVim,0x7D8810EB,0x3280,0x4D01,0xBA,0xF2,0x22,0x04,0x47,0x8D,0x9F,0xFD);
+
+#ifndef __ICommands_INTERFACE_DEFINED__
+#define __ICommands_INTERFACE_DEFINED__
+
+/* interface ICommands */
+/* [object][dual][oleautomation][uuid] */ 
+
+
+DEFINE_GUID(IID_ICommands,0x1E64C0E6,0x0563,0x4FDD,0xBB,0x1F,0xCB,0xFA,0xAA,0x58,0x4D,0x13);
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("1E64C0E6-0563-4FDD-BB1F-CBFAAA584D13")
+    ICommands : public IDispatch
+    {
+    public:
+        virtual /* [id] */ HRESULT STDMETHODCALLTYPE MsVimCommandMethod( void) = 0;
+        
+    };
+    
+#else 	/* C style interface */
+
+    typedef struct ICommandsVtbl
+    {
+        BEGIN_INTERFACE
+        
+        HRESULT ( STDMETHODCALLTYPE __RPC_FAR *QueryInterface )( 
+            ICommands __RPC_FAR * This,
+            /* [in] */ REFIID riid,
+            /* [iid_is][out] */ void __RPC_FAR *__RPC_FAR *ppvObject);
+        
+        ULONG ( STDMETHODCALLTYPE __RPC_FAR *AddRef )( 
+            ICommands __RPC_FAR * This);
+        
+        ULONG ( STDMETHODCALLTYPE __RPC_FAR *Release )( 
+            ICommands __RPC_FAR * This);
+        
+        HRESULT ( STDMETHODCALLTYPE __RPC_FAR *GetTypeInfoCount )( 
+            ICommands __RPC_FAR * This,
+            /* [out] */ UINT __RPC_FAR *pctinfo);
+        
+        HRESULT ( STDMETHODCALLTYPE __RPC_FAR *GetTypeInfo )( 
+            ICommands __RPC_FAR * This,
+            /* [in] */ UINT iTInfo,
+            /* [in] */ LCID lcid,
+            /* [out] */ ITypeInfo __RPC_FAR *__RPC_FAR *ppTInfo);
+        
+        HRESULT ( STDMETHODCALLTYPE __RPC_FAR *GetIDsOfNames )( 
+            ICommands __RPC_FAR * This,
+            /* [in] */ REFIID riid,
+            /* [size_is][in] */ LPOLESTR __RPC_FAR *rgszNames,
+            /* [in] */ UINT cNames,
+            /* [in] */ LCID lcid,
+            /* [size_is][out] */ DISPID __RPC_FAR *rgDispId);
+        
+        /* [local] */ HRESULT ( STDMETHODCALLTYPE __RPC_FAR *Invoke )( 
+            ICommands __RPC_FAR * This,
+            /* [in] */ DISPID dispIdMember,
+            /* [in] */ REFIID riid,
+            /* [in] */ LCID lcid,
+            /* [in] */ WORD wFlags,
+            /* [out][in] */ DISPPARAMS __RPC_FAR *pDispParams,
+            /* [out] */ VARIANT __RPC_FAR *pVarResult,
+            /* [out] */ EXCEPINFO __RPC_FAR *pExcepInfo,
+            /* [out] */ UINT __RPC_FAR *puArgErr);
+        
+        /* [id] */ HRESULT ( STDMETHODCALLTYPE __RPC_FAR *MsVimCommandMethod )( 
+            ICommands __RPC_FAR * This);
+        
+        END_INTERFACE
+    } ICommandsVtbl;
+
+    interface ICommands
+    {
+        CONST_VTBL struct ICommandsVtbl __RPC_FAR *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define ICommands_QueryInterface(This,riid,ppvObject)	\
+    (This)->lpVtbl -> QueryInterface(This,riid,ppvObject)
+
+#define ICommands_AddRef(This)	\
+    (This)->lpVtbl -> AddRef(This)
+
+#define ICommands_Release(This)	\
+    (This)->lpVtbl -> Release(This)
+
+
+#define ICommands_GetTypeInfoCount(This,pctinfo)	\
+    (This)->lpVtbl -> GetTypeInfoCount(This,pctinfo)
+
+#define ICommands_GetTypeInfo(This,iTInfo,lcid,ppTInfo)	\
+    (This)->lpVtbl -> GetTypeInfo(This,iTInfo,lcid,ppTInfo)
+
+#define ICommands_GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)	\
+    (This)->lpVtbl -> GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)
+
+#define ICommands_Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)	\
+    (This)->lpVtbl -> Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)
+
+
+#define ICommands_MsVimCommandMethod(This)	\
+    (This)->lpVtbl -> MsVimCommandMethod(This)
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+/* [id] */ HRESULT STDMETHODCALLTYPE ICommands_MsVimCommandMethod_Proxy( 
+    ICommands __RPC_FAR * This);
+
+
+void __RPC_STUB ICommands_MsVimCommandMethod_Stub(
+    IRpcStubBuffer *This,
+    IRpcChannelBuffer *_pRpcChannelBuffer,
+    PRPC_MESSAGE _pRpcMessage,
+    DWORD *_pdwStubPhase);
+
+
+
+#endif 	/* __ICommands_INTERFACE_DEFINED__ */
+
+
+DEFINE_GUID(CLSID_Commands,0xCDD98441,0x4E15,0x4618,0xA0,0x95,0xDB,0x52,0x91,0xE7,0x58,0x6B);
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("CDD98441-4E15-4618-A095-DB5291E7586B")
+Commands;
+#endif
+
+DEFINE_GUID(CLSID_ApplicationEvents,0xA3EF0883,0xAE9B,0x41CA,0x84,0x95,0xA2,0x1C,0xA6,0xD7,0x2C,0x91);
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("A3EF0883-AE9B-41CA-8495-A21CA6D72C91")
+ApplicationEvents;
+#endif
+
+DEFINE_GUID(CLSID_DebuggerEvents,0xA4F1F6ED,0xF5CF,0x4FF2,0x80,0x80,0x00,0x4A,0x7E,0x05,0x96,0x0F);
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("A4F1F6ED-F5CF-4FF2-8080-004A7E05960F")
+DebuggerEvents;
+#endif
+#endif /* __MsVim_LIBRARY_DEFINED__ */
+
+/* Additional Prototypes for ALL interfaces */
+
+/* end of Additional Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
